f458367ef139b8b7bf1f1ebcef4a39fad0c36eba
MC <chokamoses2@gmail.com>
2023-02-20 20:07:17 +0300

    Added themes on the BMI app

--- BMI-Calculator.R
+++ BMI-Calculator.R
@@ -8,7 +8,7 @@ library(babynames)
 # create the ui
 ui <- fluidPage(
   titlePanel("BMI Calculator"),
-  shinythemes::themeSelector(),
+  theme = shinythemes::shinytheme('darkly'),
   
   sidebarLayout(
     sidebarPanel(
@@ -17,16 +17,32 @@ ui <- fluidPage(
     ),
     mainPanel(
       textOutput('bmi'),
-      textOutput('bmi_range'),
-      textOutput("comment")
+      textOutput('bmi_range')
     )
   )
 )
 
 server <- function(input, output, session){
-  output$comment <- renderText({
-    paste("Your current weight is" ,input$weight , "and height is", input$height)
-  })
+  
+ rval_bmi <- reactive({
+   input$weight/(input$height^2)
+   })
+ 
+ output$bmi <- renderText({
+   bmi <- rval_bmi(
+     
+   )
+   paste("Your current BMI is : ", round(bmi, 1))
+ }) 
+ 
+ output$bmi_range <- renderText({
+   bmi <- rval_bmi()
+   bmi_status <- cut(rval_bmi(),
+                     breaks = c(0, 18.5, 24.9, 29.9,40),
+                     labels = c('underweight','healthy','overweight','obese')
+                     )
+   paste("You are", bmi_status)
+ })
 }
 
 shinyApp(ui = ui, server = server)
